11ef76f9e7ccb38c32503af371d16378e6145c6e
ejuniorls <ejuniorls@outlook.com>
2022-01-04 00:07:24 -0300

    inserido slug nas categorias

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -12,6 +13,8 @@ class Category extends Model
 
     protected $fillable = [
         'name',
+        'slug',
+        'description',
         'image'
     ];
 
@@ -20,6 +23,13 @@ class Category extends Model
         'updated_at',
         'deleted_at'
     ];
+
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            $category->slug = Str::slug($category->name);
+        });
+    }
 
     public function products()
     {
